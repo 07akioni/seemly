f939898882ec4ba641ae0b00fb4ad53b48243971
07akioni <07akioni2@gmail.com>
2021-04-10 20:24:00 +0800

    refactor: hsl & hsv sl & sl is 0 ~ 100

--- src/color/convert.ts
+++ src/color/convert.ts
@@ -2,51 +2,52 @@ import { RGB, HSV, HSL } from '.'
 
 /**
  * @param h 360
- * @param s 1
- * @param l 1
- * @returns [h, s, v] 360, 1, 1
+ * @param s 100
+ * @param l 100
+ * @returns [h, s, v] 360, 100, 100
  */
-export const hsl2hsv = (
-  h: number,
-  s: number,
-  l: number,
-  v = s * Math.min(l, 1 - l) + l
-): HSV => [h, v ? 2 - (2 * l) / v : 0, v]
+export function hsl2hsv(h: number, s: number, l: number): HSV {
+  s /= 100
+  l /= 100
+  const v = s * Math.min(l, 1 - l) + l
+  return [h, v ? (2 - (2 * l) / v) * 100 : 0, v * 100]
+}
 
 /**
  * @param h 360
- * @param s 1
- * @param v 1
- * @returns [h, s, l] 360, 1, 1
+ * @param s 100
+ * @param v 100
+ * @returns [h, s, l] 360, 100, 100
  */
-export const hsv2hsl = (
-  h: number,
-  s: number,
-  v: number,
-  l = v - (v * s) / 2,
-  m = Math.min(l, 1 - l)
-): HSL => [h, m ? (v - l) / m : 0, l]
+export function hsv2hsl(h: number, s: number, v: number): HSL {
+  s /= 100
+  v /= 100
+  const l = v - (v * s) / 2
+  const m = Math.min(l, 1 - l)
+  return [h, m ? ((v - l) / m) * 100 : 0, l * 100]
+}
 
 /**
  * @param h 360
- * @param s 1
- * @param v 1
+ * @param s 100
+ * @param v 100
  * @returns [r, g, b] 255, 255, 255
  */
 export function hsv2rgb(h: number, s: number, v: number): RGB {
+  s /= 100
+  v /= 100
   let f = (n: number, k = (n + h / 60) % 6) =>
     v - v * s * Math.max(Math.min(k, 4 - k, 1), 0)
   return [f(5) * 255, f(3) * 255, f(1) * 255]
 }
 
 /**
- *
  * @param r 255
  * @param g 255
  * @param b 255
- * @returns [360, 1, 1]
+ * @returns [360, 100, 100]
  */
-export function rgb2hsv (r: number, g: number, b: number): HSV {
+export function rgb2hsv(r: number, g: number, b: number): HSV {
   r /= 255
   g /= 255
   b /= 255
@@ -54,5 +55,5 @@ export function rgb2hsv (r: number, g: number, b: number): HSV {
     c = v - Math.min(r, g, b)
   let h =
     c && (v == r ? (g - b) / c : v == g ? 2 + (b - r) / c : 4 + (r - g) / c)
-  return [60 * (h < 0 ? h + 6 : h), v && c / v, v]
+  return [60 * (h < 0 ? h + 6 : h), v && (c / v) * 100, v * 100]
 }
--- src/color/index.spec.ts
+++ src/color/index.spec.ts
@@ -1,4 +1,27 @@
-import { getAlpha, toRgbString, getAlphaString, hsla, hsva } from '../index'
+import {
+  getAlpha,
+  toRgbString,
+  getAlphaString,
+  hsla,
+  hsva,
+  hsv2hsl,
+  hsv2rgb,
+  rgb2hsv,
+  hsl2hsv
+} from '../index'
+
+function round (arr: number[]) {
+  return arr.map(v => Math.round(v))
+}
+
+describe('# convert', () => {
+  // https://convertingcolors.com/
+  // rgb(167, 64, 174)
+  expect(round(hsv2hsl(296, 63, 68))).toEqual([296, 46, 47])
+  expect(round(hsl2hsv(296, 46, 47))).toEqual([296, 63, 69])
+  expect(round(rgb2hsv(167, 64, 174))).toEqual([296, 63, 68])
+  expect(round(hsv2rgb(296, 63, 68))).toEqual([169, 65, 176])
+})
 
 describe('# getAlpha', () => {
   it('rgb', () => {
@@ -28,41 +51,20 @@ describe('# toRgbString', () => {
   })
 })
 
-
 describe('# hsla', () => {
   it('hsa', () => {
-    expect(hsla('hsla(180, 50%, 60%, 0.5)')).toEqual([
-      180,
-      50,
-      60,
-      0.5
-    ])
+    expect(hsla('hsla(180, 50%, 60%, 0.5)')).toEqual([180, 50, 60, 0.5])
   })
   it('hsla', () => {
-    expect(hsla('hsl(180, 50%, 60%)')).toEqual([
-      180,
-      50,
-      60,
-      1
-    ])
+    expect(hsla('hsl(180, 50%, 60%)')).toEqual([180, 50, 60, 1])
   })
 })
 
 describe('# hsva', () => {
   it('hsv', () => {
-    expect(hsva('hsv(180, 50%, 60%)')).toEqual([
-      180,
-      50,
-      60,
-      1
-    ])
+    expect(hsva('hsv(180, 50%, 60%)')).toEqual([180, 50, 60, 1])
   })
   it('hsva', () => {
-    expect(hsva('hsva(180, 50%, 60%, 0.5)')).toEqual([
-      180,
-      50,
-      60,
-      0.5
-    ])
+    expect(hsva('hsva(180, 50%, 60%, 0.5)')).toEqual([180, 50, 60, 0.5])
   })
 })
