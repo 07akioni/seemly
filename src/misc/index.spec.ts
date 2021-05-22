517ee2b868d60bf3b52269411bf333628c6737c5
07akioni <07akioni2@gmail.com>
2021-05-22 16:14:22 +0800

    refactor(repeat, indexMap): move count to first param

--- src/misc/index.spec.ts
+++ src/misc/index.spec.ts
@@ -3,11 +3,12 @@ import { repeat, indexMap } from '..'
 describe('#misc', () => {
   describe('repeat()', () => {
     it('works', () => {
-    expect(repeat(1, 5)).toEqual([1, 1, 1, 1, 1])
+    expect(repeat(5, 1)).toEqual([1, 1, 1, 1, 1])
 
     })
   })
   describe('indexMap()', () => {
-    expect(indexMap(v => v, 5)).toEqual([0, 1, 2, 3, 4])
+    expect(indexMap(5, v => v + 1)).toEqual([1, 2, 3, 4, 5])
+    expect(indexMap(5)).toEqual([0, 1, 2, 3, 4])
   })
 })
